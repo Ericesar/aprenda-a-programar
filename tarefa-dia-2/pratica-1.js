b167eb215326b2d4964046728160cbf43b3164fd
eric cesar <ericesar.ferreira@gmail.com>
2022-08-28 23:54:15 -0300

    texto de vizualização - viagens dos sonhos

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
-let Nome = "Eric Cesar" 
-let Email = "ericesar.ferreira@gmail.com"
-let Profissão = "Estoquista" 
-let Idade = "29"
-let Cidade = "Los Santos"
+let nome = "Eric Cesar" 
+let email = "ericesar.ferreira@gmail.com"
+let profissao = "Estoquista" 
+let idade = 29
+let viagemDosSonhos = "Los Santos"
+
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+
+console.log(mensagem)
